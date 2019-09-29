94f5440cf6bc60feb657bc6a057d1fe64e829500
hieumaestro <54903623+hieumaestro@users.noreply.github.com>
2019-09-29 14:20:21 +0700

    Create index.js
    Make it better

---
+++ index.js
@@ -0,0 +1,4 @@
+// Mảng được sử dụng để lưu trị 1 list các phần tử giống nhau, 
+// cùng kiểu dữ liệu, có thể là 1 list các object có các thuộc
+// tính giống nhau. Object được sử dụng để mô tả 1 thực thể gồm
+// nhiều thuộc tính khác nhau.
